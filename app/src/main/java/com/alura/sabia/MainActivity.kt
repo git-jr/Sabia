6c7df1e98faf83a62843c2b3ab4aba138a338bb7
git-jr <juniorobom01@gmail.com>
2025-02-13 16:53:08 -0300

    aula 1.4

--- app/src/main/java/com/alura/sabia/MainActivity.kt
+++ app/src/main/java/com/alura/sabia/MainActivity.kt
@@ -6,9 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.compose.rememberNavController
+import com.alura.sabia.gemini.GeminiAPI
 import com.alura.sabia.ui.navigation.NavHost
 import com.alura.sabia.ui.theme.SabiaTheme
-import com.google.ai.client.generativeai.GenerativeModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
@@ -18,17 +18,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        val apiKey = BuildConfig.apiKey
-        println("apiKey: $apiKey")
 
-        val model = GenerativeModel(
-            "gemini-2.0-flash",
-            apiKey
-        )
+        val geminiAPI = GeminiAPI()
 
         lifecycleScope.launch {
-            val response = model.generateContent("Quando o Brasil foi descoberto?")
-            println("response: ${response.text}")
+           geminiAPI.generateContent("Quando o Android foi lançado?")
         }
 
         setContent {
---
+++ app/src/main/java/com/alura/sabia/gemini/GeminiAPI.kt
@@ -0,0 +1,22 @@
+package com.alura.sabia.gemini
+
+import android.util.Log
+import com.alura.sabia.BuildConfig
+import com.google.ai.client.generativeai.GenerativeModel
+
+class GeminiAPI(
+    apiKey: String = BuildConfig.apiKey,
+    modelName: String = "gemini-2.0-flash"
+) {
+    private var model: GenerativeModel
+
+    init {
+        model = GenerativeModel(modelName, apiKey)
+    }
+
+    suspend fun generateContent(prompt: String): String? {
+        val response = model.generateContent(prompt)
+        Log.d("GeminiAPI", "response: ${response.text}")
+        return response.text
+    }
+}
